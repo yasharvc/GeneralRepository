9382f1d90b273b682dc0a64ddab58a118a228675
Yashar Aliabbasi <yashar@pragmateam.dev>
2020-11-28 22:45:19 +0330

    Added extension function for converting Json string into dictionary of string to object

---
+++ GeneralRepository/src/Business/Core/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Extensions
+{
+	public static class StringExtensions
+	{
+		public static IDictionary<string,object> ToDictionary(this string json)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ GeneralRepository/test/UnitTests/InputDeserializingTests/JsonStringInputTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.InputDeserializingTests
+{
+	public class JsonStringInputTests
+	{
+		[Fact]
+		public void ToDictionary_WithInvalidJson_ShouldThrowInvalidInputException()
+		{
+
+		}
+	}
+}

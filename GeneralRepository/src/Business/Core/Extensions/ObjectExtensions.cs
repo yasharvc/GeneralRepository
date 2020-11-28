648eeccf1c4c9a77c79e4869c17605378af93a7e
Yashar Aliabbasi <yashar@pragmateam.dev>
2020-11-29 02:01:15 +0330

    Handling basic type properties and basic object type objects inside json

---
+++ GeneralRepository/src/Business/Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Core.Exceptions.Application;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Core.Extensions
+{
+	public static class ObjectExtensions
+	{
+		public static IDictionary<string, object> ToGeneralDictionary(this object obj)
+		{
+			try
+			{
+				return new ObjectToDictionaryConverter(obj).Translate();
+			}
+			catch (JsonException)
+			{
+				throw new InvalidJsonStringInputException();
+			}
+		}
+
+		private class ObjectToDictionaryConverter
+		{
+			public IDictionary<string,JsonElement> Elements { get; set; }
+			public ObjectToDictionaryConverter(object obj)
+			{
+				Elements = GetLowLevelDictionary(obj);
+			}
+
+			public IDictionary<string,object> Translate()
+			{
+				var result = new Dictionary<string, object>();
+				foreach (var item in Elements)
+					result[item.Key] = JsonElementToValue(item.Value);
+				return result;
+			}
+
+			private static IDictionary<string, JsonElement> GetLowLevelDictionary(object obj)
+			{
+				if (obj.GetType() == typeof(string))
+					return JsonSerializer.Deserialize<IDictionary<string, JsonElement>>(obj as string);
+				else
+					return JsonSerializer.Deserialize<IDictionary<string, JsonElement>>(JsonSerializer.Serialize(obj));
+			}
+
+			private static object JsonElementToValue(JsonElement value)
+			{
+				return value.ValueKind switch
+				{
+					JsonValueKind.String => value.GetString(),
+					JsonValueKind.False or JsonValueKind.True => value.GetBoolean(),
+					JsonValueKind.Number => value.GetDouble(),
+					JsonValueKind.Null or JsonValueKind.Undefined => null,
+					JsonValueKind.Object => new ObjectToDictionaryConverter(value.GetRawText().ToGeneralDictionary()).Translate(),
+					_ => throw new NotImplementedException(),
+				};
+			}
+		}
+	}
+}
--- GeneralRepository/test/UnitTests/InputDeserializingTests/JsonStringInputTests.cs
+++ GeneralRepository/test/UnitTests/InputDeserializingTests/JsonStringInputTests.cs
@@ -1,16 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Core.Exceptions.Application;
+using Core.Extensions;
+using System;
+using System.Text.Json;
 using Xunit;
 
 namespace UnitTests.InputDeserializingTests
 {
 	public class JsonStringInputTests
 	{
+		readonly object TestObject = new {
+			name = "name",
+			age = 24,
+			address = new { city = "Tabriz", country = "Iran" },
+			items = new object[] {
+				new { id = 1, title = "Stuff 1" },
+				new { id = 2, title = "Stuff 2" }
+			}
+		};
+
+		class TestClass
+		{
+			public string Name { get; set; }
+			public int Age { get; set; }
+			public DateTime? DOB { get; set; }
+		}
+
+		readonly string TestObjectJson = "";
+		public JsonStringInputTests()
+		{
+			TestObjectJson = JsonSerializer.Serialize(TestObject);
+		}
 		[Fact]
 		public void ToDictionary_WithInvalidJson_ShouldThrowInvalidInputException()
 		{
+			var input = TestObjectJson[2..];
 
+			Assert.Throws<InvalidJsonStringInputException>(() => input.ToGeneralDictionary());
+		}
+
+		[Fact]
+		public void ToDictionary_WithValidJson_ShouldReturnValidDictionary()
+		{
+			var input = TestObjectJson;
+			
+			var obj = input.ToGeneralDictionary();
+
+			Assert.True(obj.ContainsKey("name"));
+			Assert.True(obj.ContainsKey("age"));
+			Assert.True(obj.ContainsKey("address"));
+			Assert.True(obj.ContainsKey("items"));
+		}
+
+		[Fact]
+		public void ToDictionary_WithTestClassObject_ShouldReturnValidDictionary()
+		{
+			var obj = new TestClass
+			{
+				Name = "name",
+				Age = 34,
+				DOB = DateTime.Now.AddYears(-34)
+			};
+
+			var dictionary = obj.ToGeneralDictionary();
+
+			Assert.True(dictionary.ContainsKey(nameof(TestClass.Name)));
+			Assert.True(dictionary.ContainsKey(nameof(TestClass.Age)));
+			Assert.True(dictionary.ContainsKey(nameof(TestClass.DOB)));
+
+			Assert.Equal(obj.Name,dictionary[nameof(TestClass.Name)] as string);
+			Assert.Equal(obj.Age, Convert.ToInt32(dictionary[nameof(TestClass.Age)]));
+			Assert.Equal(obj.DOB, Convert.ToDateTime(dictionary[nameof(TestClass.DOB)]));
 		}
 	}
 }

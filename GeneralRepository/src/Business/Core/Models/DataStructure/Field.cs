4a285dc266207aa99b2f6f962d77a1ab8ca2140c
Yashar Aliabbasi <yashar@pragmateam.dev>
2020-11-30 21:21:10 +0330

    Added basic properties of structure of data

---
+++ GeneralRepository/src/Business/Core/Models/DataStructure/Field.cs
@@ -0,0 +1,15 @@
+﻿using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.DataStructure
+{
+	public class Field : Model
+	{
+		public BasicDataTypeEnum DataType { get; set; }
+		public string MyProperty { get; set; }
+	}
+}

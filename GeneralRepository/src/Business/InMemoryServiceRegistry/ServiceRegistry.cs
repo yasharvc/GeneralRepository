f3f959a4e05d407b71a2f80fb560b004fdf42941
Yashar Aliabbasi <yashar@pragmateam.dev>
2020-12-05 20:27:03 +0330

    Created InMemoryServiceRegistry project

---
+++ GeneralRepository/src/Business/InMemoryServiceRegistry/ServiceRegistry.cs
@@ -0,0 +1,39 @@
+﻿using Core.Models.Service;
+using ServiceRegistry.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InMemoryServiceRegistry
+{
+	public class ServiceRegistry : IServiceRegistry
+	{
+		public event EventHandler<ServiceProfile> OnServiceRegister;
+		public event EventHandler<Guid> OnServiceUnregister;
+
+		public Task<IEnumerable<ServiceProfile>> GetAllServices()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ServiceProfile> GetServiceByRegisteredId(Guid registeredId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<ServiceProfile>> GetServicesByObjectID(string objectID)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Guid> Register(ServiceProfile service)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> Unregister(Guid registeredId)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

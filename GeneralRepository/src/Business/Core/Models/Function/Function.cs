c53677f36888fd99083a4cd431ef7a9427a73605
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-01-02 18:23:22 +0330

    Corrected Parameter field of function

--- GeneralRepository/src/Business/Core/Models/Function/Function.cs
+++ GeneralRepository/src/Business/Core/Models/Function/Function.cs
@@ -7,7 +7,7 @@ namespace Core.Models.Function
 	{
 		public string Name { get; set; }
 		public StructureDefinition ReturnType { get; set; }
-		public IEnumerable<StructureDefinition> Parameters { get; set; }
+		public StructureDefinition Parameters { get; set; }
 		public string CallPath { get; set; }
 	}
 }
